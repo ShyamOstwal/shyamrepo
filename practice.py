5e76a9a9cf6fdbcbe574ec4a61886744441b0b70
Shyam Ostwal <shyamostwal@gmail.com>
2025-01-05 18:22:52 +0530

    code in new file

---
+++ practice.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+if a>b:
+    result1=a-b 
+    print("The subtraction of a and b is:", result1)
+else:
+    result2=b-a
+    print("The subtraction of b and a is:", result2)
